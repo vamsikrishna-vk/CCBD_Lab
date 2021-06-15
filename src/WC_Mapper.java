7afcd8124e882e2237335a166883c413ccb1ba44
vamsi krishna <r18cs273@cit.reva.edu.in>
2021-06-15 14:07:23 +0530

    program 7

---
+++ src/WC_Mapper.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+public class WC_Mapper extends MapReduceBase implements
+        Mapper<LongWritable,Text,Text,IntWritable> {
+    public void map(LongWritable key, Text value,OutputCollector<Text,IntWritable> output,
+                    Reporter reporter) throws IOException{
+        String line = value.toString();
+        String tokenizer[] = line.split("");
+        for(String SingleChar : tokenizer)
+        {
+            Text charKey = new Text(SingleChar);
+            IntWritable One = new IntWritable(1);
+            output.collect(charKey, One);
+        }
+    }
+
+}
